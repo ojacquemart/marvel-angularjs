74a25e0f6ed6b94d7c0eb305da45f818e801854e
ojacquemart <o.jacquemart@gmail.com>
2014-03-02 09:33:49 +0100

    Remove Restangular overriden configuration, now use MarvelRestangular.

--- src/app/app.js
+++ src/app/app.js
@@ -4,40 +4,24 @@ angular.module('marvel.app', [
         'marvel.config',
         'marvel.common'
     ])
-    .config([ '$routeProvider', 'MarvelConfig', 'RestangularProvider', function ($routeProvider, MarvelConfig, RestangularProvider) {
-        function setRoutes() {
-            $routeProvider
-                .when('/battle', {
-                    controller: 'BattleCtrl',
-                    templateUrl: 'app/battle/battle.html'
-                })
-                .when('/characters', {
-                    controller: 'CharactersCtrl',
-                    templateUrl: 'app/characters/characters.html'
-                })
-                .when('/comics', {
-                    controller: 'ComicsCtrl',
-                    templateUrl: 'app/comics/comics.html'
-                })
-                .otherwise({
-                    redirectTo: '/battle'
-                });
-        };
-
-        function setRestangularDefaultConfig() {
-            RestangularProvider.setBaseUrl(MarvelConfig.URL);
-
-            var timestamp = Date.now();
-            var hash = md5(timestamp + MarvelConfig.PRIVATE_KEY + MarvelConfig.PUBLIC_KEY);
-            RestangularProvider.setDefaultRequestParams('get', {
-                ts: timestamp,
-                apikey: MarvelConfig.PUBLIC_KEY,
-                hash: hash
+    .config([ '$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/battle', {
+                controller: 'BattleCtrl',
+                templateUrl: 'app/battle/battle.html'
+            })
+            .when('/characters', {
+                controller: 'CharactersCtrl',
+                templateUrl: 'app/characters/characters.html'
+            })
+            .when('/comics', {
+                controller: 'ComicsCtrl',
+                templateUrl: 'app/comics/comics.html'
+            })
+            .otherwise({
+                redirectTo: '/battle'
             });
-        };
 
-        setRoutes();
-        setRestangularDefaultConfig();
     }])
     .run(['$rootScope', '$location', function ($rootScope, $location) {
     }])
