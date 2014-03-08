f49cf0c670270a28e354224aee5407046a9fb3b0
ojacquemart <o.jacquemart@gmail.com>
2014-03-08 10:09:07 +0100

    Add battle api spec.

---
+++ src/app/battle/battle-api.srv.spec.js
@@ -0,0 +1,16 @@
+describe('service: BattleApi', function() {
+
+    beforeEach(module('marvel.common'));
+    beforeEach(module('marvel.app'));
+
+    describe('When asking for a character', function() {
+        it('should call the MarvelRestangular provider', inject(function(MarvelRestangular, BattleApi) {
+            spyOn(MarvelRestangular, 'one').andCallThrough();
+
+            BattleApi.getCharacter(0);
+
+            expect(MarvelRestangular.one).toHaveBeenCalledWith('characters');
+        }));
+    })
+})
+;
